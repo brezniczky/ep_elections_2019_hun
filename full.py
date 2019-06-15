f3dcab028989582c61c43e0d288387d5384ec6cf
Janos Brezniczky <brezniczky@gmail.com>
2019-06-16 01:37:17 +0200

    New non-param test: prob. of avg. max_to_mean of top most skewed

--- full.py
+++ full.py
@@ -25,8 +25,9 @@ MMT; WT
 """
 
 
-MAX_TO_MEAN_THRESHOLD = 1.5
-WARD_THRESHOLD = 20
+MAX_TO_MEAN_THRESHOLD = 2.0
+# WARD_THRESHOLD = 20
+MIN_VOTERS_THRESHOLD = 100
 
 
 if not os.path.exists("merged.csv"):
@@ -58,16 +59,21 @@ df.columns = [
     "MSZP", "MKKP", "Jobbik", "Fidesz", "Momentum", "DK", "Mi Hazank", "Munkaspart", "LMP"
 ]
 
+# TODO: revise this
+# There is a NAN line at the end, remove
+df.drop(np.where(np.isnan(df.Ervenyes))[0], inplace=True)
+
 columns = df.columns.copy()
 
 for column in columns[-10:]:
     df["ld_" + column] = df[column] % 10
+df["ld_Nevjegyzekben"] = df["Nevjegyzekben"] % 10
 
-town_groups = df[["Megye", "Telepules", "ld_Fidesz"]].groupby(["Megye", "Telepules", "ld_Fidesz"])
-county_town_digit_sums = town_groups.aggregate(len)
-
-# TODO: digit_sums is unused
-digit_sums = county_town_digit_sums.groupby(["ld_Fidesz"]).aggregate(len)
+county_town_digit_groups = (
+    df[["Megye", "Telepules", "ld_Fidesz", "Fidesz"]]
+    .groupby(["Megye", "Telepules", "ld_Fidesz"])
+)
+county_town_digit_sums = county_town_digit_groups.aggregate({"ld_Fidesz": len, "Fidesz": min})
 
 
 def lucky_nr(occurrances):
@@ -75,26 +81,39 @@ def lucky_nr(occurrances):
                        for x in
                        list(occurrances.index)])
     counts = np.array(list(occurrances))
-    return digits[counts==max(counts)][0]
+    tops = digits[counts==max(counts)]
+    if len(tops) == 1:
+        return tops[0]
+        # return np.random.choice(tops, 1)[0]
+    else:
+        return None
 
 
 def lucky_nr2(occurrances):
-    """ 2nd most frequently occurring last digit """
     digits = np.array([x[-1]
                        for x in
                        list(occurrances.index)])
     counts = np.array(list(occurrances))
-    return digits[counts==sorted(counts)[1]][0] \
-           if len(occurrances) > 1 else None
+    tops = digits[counts==max(counts)]
+    if len(tops) == 1:
+        if len(counts) > 1:
+            top2s = digits[counts==(sorted(counts)[-2])]
+            if len(top2s) == 1:
+                return top2s[0]
+            # return np.random.choice(top2s, 1)[0]
+    else:
+        return None
 
 
 digit_sum_extr = \
     county_town_digit_sums.groupby(["Megye", "Telepules"]) \
-    .aggregate([max, 'mean', min, sum, lucky_nr, lucky_nr2])
+    .aggregate({"ld_Fidesz": [max, 'mean', min, sum, lucky_nr, lucky_nr2],
+                "Fidesz": min})
 
 
-digit_sum_extr["max_to_min"] = digit_sum_extr["max"] / digit_sum_extr["min"]
-digit_sum_extr["max_to_mean"] = digit_sum_extr["max"] / digit_sum_extr["mean"]
+digit_stats = digit_sum_extr.ld_Fidesz
+digit_sum_extr["ld_Fidesz", "max_to_min"] = digit_stats["max"] / digit_stats["min"]
+digit_sum_extr["ld_Fidesz", "max_to_mean"] = digit_stats["max"] / digit_stats["mean"]
 
 
 """
@@ -103,8 +122,11 @@ max_to_min >= 2 and suspects2["sum"] >= 20
 and have been removed from this script.
 """
 
-suspects2 = digit_sum_extr.loc[digit_sum_extr.max_to_mean >= MAX_TO_MEAN_THRESHOLD]
-suspects2 = suspects2.loc[suspects2["sum"] >= WARD_THRESHOLD]
+suspects2 = digit_sum_extr.loc[digit_sum_extr.ld_Fidesz.max_to_mean >= MAX_TO_MEAN_THRESHOLD]
+# suspects2 = suspects2.loc[suspects2.ld_Fidesz["sum"] >= WARD_THRESHOLD]
+suspects2 = suspects2.loc[suspects2.Fidesz["min"] >= MIN_VOTERS_THRESHOLD]
+
+suspects2 = suspects2["ld_Fidesz"]
 
 print("Suspicious areas based on max to mean digit occurrence ratio")
 
@@ -186,7 +208,7 @@ SOMOGY               Siófok                     4   2.666667    1   24         
 # Apparently, in these suspicious towns, people disfavour 7's and 1's
 print("lucky number distribution")
 
-plt.hist(digit_sum_extr.lucky_nr, bins=10)
+plt.hist(digit_sum_extr.ld_Fidesz.lucky_nr, bins=10)
 plt.title("\"Lucky number\" (most frequent last digit) distribution\n"
           "just about everywhere - note that there's no 'zero' here.")
 plt.show()
@@ -217,7 +239,7 @@ A forgiving approach - let's check the probability of any
 last digit disappearing from the distibution of those
 in the suspicious areas:
 """)
-pop_last_digit_freqs = county_town_digit_sums.groupby(["ld_Fidesz"]).aggregate(sum)
+pop_last_digit_freqs = county_town_digit_sums["ld_Fidesz"].groupby(["ld_Fidesz"]).aggregate(sum)
 Pi = [pop_last_digit_freqs[float(i)] / sum(pop_last_digit_freqs) for i in range(10)]
 P = sum((1 - Pi[i]) ** len(suspects2) for i in range(10))
 
@@ -288,8 +310,10 @@ def get_overlaps(suspects):
     return commons
 
 
-suspects2_unordered = digit_sum_extr.loc[digit_sum_extr.max_to_mean >= MAX_TO_MEAN_THRESHOLD]
-suspects2_unordered = suspects2_unordered.loc[suspects2_unordered["sum"] >= WARD_THRESHOLD]
+suspects2_unordered = digit_sum_extr.loc[digit_sum_extr.ld_Fidesz.max_to_mean >= MAX_TO_MEAN_THRESHOLD]
+# suspects2_unordered = suspects2_unordered.loc[suspects2_unordered["sum"] >= WARD_THRESHOLD]
+suspects2_unordered = suspects2_unordered.loc[suspects2_unordered.Fidesz["min"] >= MIN_VOTERS_THRESHOLD]
+suspects2_unordered = suspects2_unordered.ld_Fidesz
 
 overlaps = get_overlaps(suspects2_unordered)
 
@@ -328,16 +352,32 @@ ref_count = \
         suspects2_unordered.lucky_nr2
     )
 
-print("Further reassurance of that where it's skewed, it's not just randomly skewed....")
 
-print("Reference draws: uniform case")
-do_reference_draws(get_prob_for=ref_count)
+def get_non_na_dist(seq):
+    seq = np.array(seq)
+    seq = seq[~np.isnan(seq)]
+    unique, counts = np.unique(seq, return_counts=True)
+    return counts / sum(counts)
 
-print("Reference draws: non-uniform case (zeroes are 20% more frequent than other digits)")
-do_reference_draws(get_prob_for=ref_count, weights=[12 / 102] + [10 / 102] * 9)
 
-print("Reference draws: non-uniform case (zeroes are 100% more frequent than other digits)")
-do_reference_draws(get_prob_for=ref_count, weights=[2 / 11] + [1 / 11] * 9)
+# print("Further reassurance of that where it's skewed, it's not just randomly skewed....")
+
+# print("Reference draws: uniform case")
+# do_reference_draws(get_prob_for=ref_count)
+
+# print("Reference draws: non-uniform case (zeroes are 20% more frequent than other digits)")
+# do_reference_draws(get_prob_for=ref_count, weights=[12 / 102] + [10 / 102] * 9)
+
+# print("Reference draws: non-uniform case (zeroes are 100% more frequent than other digits)")
+# do_reference_draws(get_prob_for=ref_count, weights=[2 / 11] + [1 / 11] * 9)
+
+# print("Reference draws: non-uniform case (using empiric distribution of lucky numbers in the suspect areas)")
+# do_reference_draws(
+#     get_prob_for=ref_count,
+#     weights=get_non_na_dist(list(suspects2.lucky_nr) +
+#                      list(suspects2.lucky_nr2))
+# )
+
 
 print("The chance of not getting anything similar is well above 95% in each case.")
 
@@ -367,6 +407,123 @@ coincidence?)
 
 
 """
+We may suspect some areal influence therefore ... :
+"""
+# print("Reference draws: using empiric distribution of lucky numbers in the suspect areas")
+# do_reference_draws(
+#     get_prob_for=ref_count,
+#     weights=get_non_na_dist(list(suspects2.lucky_nr) +
+#                      list(suspects2.lucky_nr2))
+# )
+
+
+# print("Reference draws: using empiric distribution "
+#       "of lucky numbers from wards with voters >= %d" % MIN_VOTERS_THRESHOLD)
+# big_enough_wards_data = digit_sum_extr.loc[digit_sum_extr.Fidesz["min"] >=
+#                                            MIN_VOTERS_THRESHOLD]
+# do_reference_draws(
+#     get_prob_for=ref_count,
+#     weights=get_non_na_dist(list(big_enough_wards_data.ld_Fidesz.lucky_nr) +
+#                             list(big_enough_wards_data.ld_Fidesz.lucky_nr2))
+# )
+
+# dist = get_non_na_dist(big_enough_wards_data.ld_Fidesz.lucky_nr)
+# plt.bar(range(10), dist)
+# plt.title("Most frequent Fidesz votes' last digit distribution\n"
+#           "in areas where Fidesz received at least %d votes in\n"
+#           "each ward" % MIN_VOTERS_THRESHOLD)
+# plt.show()
+
+
+# dist = get_non_na_dist(big_enough_wards_data.ld_Fidesz.lucky_nr)
+# plt.bar(range(10), dist)
+# plt.title("Most frequent Fidesz votes' last digit distribution\n"
+#           "in areas where Fidesz received at least %d votes in\n"
+#           "each ward" % MIN_VOTERS_THRESHOLD)
+# plt.show()
+
+
+# def get_most_freq(seq):
+#     return np.bincount(seq).argmax()
+
+
+"""
+def get_most_freq(seq):
+    uniques, counts = np.unique(seq, return_counts=True)
+    top_ones = uniques[counts == max(counts)]
+    if len(top_ones) == 1:
+        return top_ones[0]
+    else:
+        return np.random.choice(top_ones, 1)[0]
+
+
+df2 = df[["Megye", "Telepules", "ld_Ervenyes", "Fidesz", "ld_Fidesz", "ld_Nevjegyzekben", "ld_DK", "ld_Jobbik"]]
+df2 = (df2.groupby(["Megye", "Telepules"])
+       .aggregate({"ld_Ervenyes": get_most_freq,
+                   "Fidesz": min,
+                   "ld_Fidesz": get_most_freq,
+                   "ld_DK": get_most_freq,
+                   "ld_Jobbik": get_most_freq,
+                   "ld_Nevjegyzekben": get_most_freq}))
+
+plt.hist(df2.loc[df2.Fidesz >= 100].ld_Ervenyes)
+plt.show()
+plt.hist(df2.loc[df2.Fidesz >= 100].ld_DK)
+plt.show()
+plt.hist(df2.loc[df2.Fidesz >= 100].ld_Nevjegyzekben)
+plt.show()
+
+plt.hist(df2.loc[df2.Fidesz >= 100].ld_Nevjegyzekben, alpha=0.5)
+plt.hist(df2.loc[df2.Fidesz >= 100].ld_Fidesz, alpha=0.5)
+plt.show()
+
+plt.hist(df2.loc[df2.Fidesz < 100].ld_Nevjegyzekben, alpha=0.5)
+plt.hist(df2.loc[df2.Fidesz < 100].ld_Fidesz, alpha=0.5)
+plt.show()
+
+# plt.hist(df2.loc[df2.Fidesz >= 100].ld_Nevjegyzekben, alpha=0.5)
+plt.hist(df2.loc[df2.Fidesz >= 100].ld_DK, alpha=0.5)
+plt.show()
+
+# plt.hist(df2.loc[df2.Fidesz >= 100].ld_Nevjegyzekben, alpha=0.5)
+plt.hist(df2.loc[df2.Fidesz >= 100].ld_Jobbik, alpha=0.5)
+plt.show()
+
+plt.hist(df2.loc[df2.Fidesz < 100].ld_Fidesz, alpha=0.5)
+plt.show()
+
+plt.hist(df2.loc[df2.Fidesz < 100].ld_Ervenyes, alpha=0.5)
+plt.show()
+
+df2_fb = df2[df2.Fidesz > 100]
+print(sum(df2_fb["ld_Ervenyes"] == df2_fb["ld_Fidesz"]) / len(df2_fb))
+
+"""
+
+
+""" Can it be really this skewed? Well... seriously? """
+
+"""
+this one works but is hard to explain
+
+plt.hist(big_enough_wards_data.Fidesz["min"] % 10, bins=10)
+
+plt.show()
+"""
+
+"""
+Get the distribution of ward size last digits where the minimum ward size is at least 10.
+How do you go about it?
+Can involve a join, for instance.
+
+I guess I can just use "valid votes" ("Ervenyes") for this too.
+There will not be any visible skew anyway.
+"""
+
+
+
+
+"""
 The author is aware of local concerns about the cleanness of
 the electoral procedures in at least a few of these regions.
 
@@ -383,4 +540,221 @@ from better processes and a closer supervision.
 plt.scatter(digit_sum_extr["sum"], digit_sum_extr.max_to_mean, alpha=0.5)
 plt.scatter(suspects2["sum"], suspects2.max_to_mean, alpha=0.5)
 plt.show()
+"""
+
+
+
+"""
+How will I recover from this?
+So - I'll stop doing it. Must stop.
+Burns time and money. I have neither.
+
+Ende.
+
+What can we do?
+
+Still I don't believe in the Fidesz voters.
+Nobody sees them. Nobody knows them.
+The lady stressed.
+Difficult to get an answer from.
+
+12 x 9 in a single settlement.
+
+There's a reason. There has to be.
+
+So I think
+- still there is plausibilty behind
+  these areas being suspects
+- there are extremely suspicious skews
+  (12 9's in Eger)
+-
+
+I could however check if the number of
+
+sqrt(sum(max_to_mean ^ 2))
+
+
+"max_to_mean" threshold breakers can come
+from a uniform distribution.
+
+
+1 - sum(np.isnan(suspects2.lucky_nr)) / len(suspects2)
+
+
+"""
+
+# okay, let's simulate one more (one last) time
+
+actual_sum_max_to_mean = sum(suspects2.max_to_mean)
+actual_mean_max_to_mean = np.mean(suspects2.max_to_mean)
+print(actual_sum_max_to_mean, actual_mean_max_to_mean)
+
+
+np.random.seed(1235)
+
+N_ITERATIONS = 1000
+
+def simulate_again(df_raw, actual_sum_max_to_mean, actual_mean_max_to_mean):
+    hits_sum = 0
+    hits_max_to_mean = 0
+    for i in range(N_ITERATIONS):
+        df = df_raw.copy()
+        df.ld_Fidesz = np.random.choice(range(10), len(df))
+
+        county_town_digit_groups = (
+            df[["Megye", "Telepules", "ld_Fidesz", "Fidesz"]]
+            .groupby(["Megye", "Telepules", "ld_Fidesz"])
+        )
+        county_town_digit_sums = county_town_digit_groups.aggregate({"ld_Fidesz": len, "Fidesz": min})
+
+        digit_sum_extr = \
+            county_town_digit_sums.groupby(["Megye", "Telepules"]) \
+            .aggregate({"ld_Fidesz": [max, 'mean'], #  , min, sum, lucky_nr, lucky_nr2],
+                        "Fidesz": min})
+
+        digit_stats = digit_sum_extr.ld_Fidesz
+        digit_sum_extr["ld_Fidesz", "max_to_mean"] = digit_stats["max"] / digit_stats["mean"]
+
+        """
+        Note: first 'suspects' were considered too fragile, based on
+        max_to_min >= 2 and suspects2["sum"] >= 20
+        and have been removed from this script.
+        """
+
+        suspects2 = digit_sum_extr.loc[digit_sum_extr.ld_Fidesz.max_to_mean >= MAX_TO_MEAN_THRESHOLD]
+        # suspects2 = suspects2.loc[suspects2.ld_Fidesz["sum"] >= WARD_THRESHOLD]
+        suspects2 = suspects2.loc[suspects2.Fidesz["min"] >= MIN_VOTERS_THRESHOLD]
+
+        suspects2 = suspects2["ld_Fidesz"]
+
+        if actual_sum_max_to_mean < sum(suspects2.max_to_mean):
+            hits_sum += 1
+        if actual_mean_max_to_mean < np.mean(suspects2.max_to_mean):
+            hits_max_to_mean += 1
+        print(sum(suspects2.max_to_mean), np.mean(suspects2.max_to_mean))
+    print(hits_sum / N_ITERATIONS * 100)
+    print(hits_max_to_mean / N_ITERATIONS * 100)
+
+simulate_again(df, actual_sum_max_to_mean, actual_mean_max_to_mean)
+
+# gives 3.7%, 3.5% for the actual_mean_max_to_mean value
+# the sum would be fine (45% p-value)
+# so what?
+
+
+
+# voter threshold: 100
+
+# n_iter, top -> seed=1234, 1235, 1236
+# 100, 50 -> 21%
+# 100, 100 -> 22%
+# 5000, 100 -> 18.34%, 19.08%, 18.46%
+# 100, 200 -> 17%
+# 100, 300 -> 7%
+# 200, 300 -> 8.5%, 9%, 7.5%
+# 100, 400 -> 11%, 7%, 9%
+# 100, 500 -> 11%, 7%, 9%
+# 1000, 500 -> 7%, 8.5%, 8%
+# 1000, 1000 -> 7%, 8.5%, 8%
+# 1000, 2000 -> 7%, 8.5%, 8%
+# 2000, 500 -> 6.85%, 7.35%, 7.65%
+# 3000, 500 -> 7.2%, 7.4%, 7.2%
+# 5000, 500 -> 7.2%, 7.4%, 7.2%
+# 5000, 1000 -> 7.02%, 7.24%, 6.48%
+
+
+# suspects2 = digit_sum_extr.loc[digit_sum_extr.ld_Fidesz.max_to_mean >= MAX_TO_MEAN_THRESHOLD]
+# # suspects2 = suspects2.loc[suspects2.ld_Fidesz["sum"] >= WARD_THRESHOLD]
+
+def simulate_mean_max_to_mean_of_top_dist(df_raw, actual_sum_max_to_mean, actual_mean_max_to_mean):
+    print()  # prepare for downcount
+    hits_sum = 0
+    hits_max_to_mean = 0
+    df = df_raw.copy()
+    start = datetime.now()
+    print("N_ITERATIONS", N_ITERATIONS)
+    for i in range(N_ITERATIONS):
+        df.ld_Fidesz = np.random.choice(range(10), len(df))
+
+        county_town_digit_groups = (
+            df[["Megye", "Telepules", "ld_Fidesz", "Fidesz"]]
+            .groupby(["Megye", "Telepules", "ld_Fidesz"])
+        )
+        county_town_digit_sums = county_town_digit_groups.aggregate({"ld_Fidesz": len, "Fidesz": min})
+
+        digit_sum_extr = \
+            county_town_digit_sums.groupby(["Megye", "Telepules"]) \
+            .aggregate({"ld_Fidesz": [max, 'mean'], #  , min, sum, lucky_nr, lucky_nr2],
+                        "Fidesz": min})
+
+        digit_stats = digit_sum_extr.ld_Fidesz
+        digit_sum_extr[("ld_Fidesz", "max_to_mean")] = digit_stats["max"] / digit_stats["mean"]
+
+        """
+        Note: first 'suspects' were considered too fragile, based on
+        max_to_min >= 2 and suspects2["sum"] >= 20
+        and have been removed from this script.
+        """
+
+        suspects3 = digit_sum_extr.loc[digit_sum_extr.Fidesz["min"] >= MIN_VOTERS_THRESHOLD]
+        suspects3 = suspects3.ld_Fidesz.sort_values(["max_to_mean"], ascending=[False])
+        suspects3 = suspects3.head(N_TOP_WEIRDEST)
+
+        if actual_sum_max_to_mean < sum(suspects3.max_to_mean):
+            hits_sum += 1
+        if actual_mean_max_to_mean < np.mean(suspects3.max_to_mean):
+            hits_max_to_mean += 1
+
+        if i % 100 == 0:
+            try:
+                dt = datetime.now() - start
+                its_per_sec = (i + 1) / dt.total_seconds()
+                print("\r%s elapsed, %.2f iterations per sec.            " %
+                      (str(dt), its_per_sec),
+                      end="")
+            except:
+                print("\r(failed to calculate iteratons per sec.)              ", end="")
+        # print(sum(suspects3.max_to_mean), np.mean(suspects3.max_to_mean))
+    print()  # otblast!
+
+    print(hits_sum / N_ITERATIONS * 100)
+    print(hits_max_to_mean / N_ITERATIONS * 100)
+    return hits_sum / N_ITERATIONS, hits_max_to_mean / N_ITERATIONS
+
+
+N_TOP_WEIRDEST = 300
+MIN_VOTERS_THRESHOLD = 100
+N_ITERATIONS = 200
+
+suspects3 = digit_sum_extr.loc[digit_sum_extr.Fidesz["min"] >= MIN_VOTERS_THRESHOLD].copy()
+suspects3 = suspects3.ld_Fidesz
+suspects3.sort_values(["max_to_mean"], ascending=[False], inplace=True)
+suspects3 = suspects3.head(N_TOP_WEIRDEST)
+
+actual_sum_max_to_mean_3 = sum(suspects3.max_to_mean)
+actual_mean_max_to_mean_3 = np.mean(suspects3.max_to_mean)
+print("actual data gave", actual_sum_max_to_mean_3, actual_mean_max_to_mean_3)
+
+results = []
+for SEED in [1234, 1235, 1236]:
+    print("simulating with seed", SEED,
+          "voters threshold", MIN_VOTERS_THRESHOLD,
+          "iterations", N_ITERATIONS,
+          ("top %d weirdest (most skewed)" % N_TOP_WEIRDEST))
+    np.random.seed(SEED)
+    # just the top 100
+    result = simulate_mean_max_to_mean_of_top_dist(
+      df, actual_sum_max_to_mean_3, actual_mean_max_to_mean_3
+    )
+    print("got probabilities", result)
+    results.append(result)
+
+
+
+
+"""
+Check back on these:
+
+MIN_VOTERS_THRESHOLD = 100
+MAX_TO_MEAN_THRESHOLD = 1.5
 """
