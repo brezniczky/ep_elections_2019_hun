2f4b321b1b7118a961d157b78051c5d955ee1563
Janos Brezniczky <brezniczky@gmail.com>
2019-06-24 11:17:30 +0100

    app5 (total ent. simulation) cleanup/foundations of a report

--- app5_var_in_top.py
+++ app5_var_in_top.py
@@ -1,87 +1,35 @@
 """
-Sum of variance of relative frequency of occurrences of digits per settlement
-being tested against this distribution of simulated values assuming a uniform
-digit distribution.
+Here particular electoral settlements will be focused on.
 
-This basically aims to tell how much the actual distribution of last digits
-is deviating
+Only those are of interest where
+- the Fidesz party got at least 100 votes - this suggests that the last digit
+  distribution should be close to uniform, which the simulation relies on
+- there are at least 10 electoral wards in the settlement - so that entropy
+  estimates are more accurate, the need to level them is less relevant
+
+When humans generate random numbers or tamper with values, the entropy is
+expected to reduce as humans tend to avoid/favour certain values.
+
+Given a simulation generating random laydowns of votes (only generating the
+last digits), how likely is it to obtain a settlement information entropy total
+as low as that of the actual 2019 data?
 """
-
-"""
-If only taking variance of the pi, the problem we face is:
-
-Eger: 5,7,6,3,2,1,4,2,5,12
-
-total_freq_var([5,7,6,3,2,1,4,2,5,12])
-Out[90]: 0.004169307378904481
-
-total_freq_var([1,2])
-Out[91]: 0.074074
-
-Even if I multiply up with n ^ (1/2) for CLT,
-
-In [93]: 0.0041693 * (sum([5,7,6,3,2,1,4,2,5,12]) ** 0.5)
-Out[93]: 0.028583280725452073
-
-is still less than
-0.1283
-
-So unlikeliness (which I try to add up or rather multiply or what, to get a joint probability)
-
-I will truly end up with a likelihood value
-
-Snells like an information entropy thing
-
-I am trying to say that the information entropy is sub-maximal with a 95% confidence?
-Am I?
-Markovians? And of what order?
-
-Eger's votes are more surprising than the below made up sequence (completely normal)
-
-"""
-
-
 import numpy as np
 from datetime import datetime
 from preprocessing import get_cleaned_data
 from digit_stat_data import get_last_digit_stats
 from scipy.stats import entropy
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 if not "df" in globals():
     df = get_cleaned_data()
-
 if not "county_town_digit_sums" in globals():
     county_town_digit_sums, digit_sum_extr = get_last_digit_stats(df)
 
 
-def total_freq_var(x):
-    # note: in the typical aggregation use case, this information
-    # could be further extracted:
-    # digits = np.array(x.index.get_level_values(2))
-
-    counts = np.array(x)
-    # remaining_digits = set(range(10)).difference(set(digits))
-
-
-    # mu = sum(counts) / min(10, len(counts))
-    # import ipdb; ipdb.set_trace()
-
-    # musq = (mu ** 2)
-    # total = sum((counts - mu) ** 2)
-    # n_remaining_digits = 10 - len(counts)
-    # total += n_remaining_digits * musq
-    sum_counts = sum(counts)
-    max_digits = min(sum_counts, 10)
-    missing_digits = max_digits - len(counts)
-    mu_p = 1 / max_digits
-    pi = counts / sum(counts)
-    var_pi = np.sum((pi - mu_p) ** 2) + (mu_p ** 2) * missing_digits
-    return var_pi / max_digits
-
-
-def total_freq_ent(x):
+def total_freq_ent(counts):
     # a) the smaller weren't giving results
     # b) the smaller are typically less interesting
     #    (politically), so in this filtering is a "proxy to"
@@ -89,7 +37,7 @@ def total_freq_ent(x):
     # c) the smaller required a conversion which I
     #    do not trust too much at this point, partly
     #    due to a)
-    if len(x) < 10:
+    if len(counts) < 10:
         return np.log(10)
     """
     In [158]: entropy([0.5, 0.5])
@@ -98,35 +46,35 @@ def total_freq_ent(x):
     In [159]: entropy([0.25, 0.25, 0.25, 0.25])
     Out[159]: 1.3862943611198906
 
-    so at the end I will level these values
+    so at the end I should level these values
     """
-    if len(x) == 1:
+    if len(counts) == 1:
         # indefinite-like form -
         # all symbols occur once, so "max"
         # but at the same time, a single symbol
-        # occurs only - is it a zero or anything else?
-        # answer: for now it's irrelevant in our analysis
-        # as lower values are of interest, push this to
-        # the "big" end
+        # occurs only, so all of them are the same -
+        # then is it a zero or anything else?
+        # my "local" answer: for now it's irrelevant in our
+        # analysis as lower values are of interest, so push
+        # this to the "big" end (unlikely a top candidate)
         return np.log(10)
-    counts = np.array(x)
-    max_digits = min(np.sum(counts), 10)
+    counts = np.array(counts)
     res = entropy(counts)
-    if max_digits != 10:
-        res = res * np.log(10) / np.log(max_digits)
+    if len(counts) < 10:
+        # this never executes now
+        max_digits_that_appear = min(np.sum(counts), 10)
+        if max_digits_that_appear != 10:
+            res = res * np.log(10) / np.log(max_digits_that_appear)
     return res
 
 
-# print(total_freq_ent([5,7,6,3,2,1,4,2,5,12]))  # Eger
-# print(total_freq_ent([5,7,6,3,2,1,4,2,12]))
-
-
-def simulate_total_var_dist(df_raw, actual_total_var):
+def get_simulated_total_ent_prob(df_raw, actual_total_ent, iterations):
     print()  # prepare for downcount
     hits = 0
     df = df_raw.copy()
     start = datetime.now()
-    for i in range(N_ITERATIONS):
+    entropies = []
+    for i in range(iterations):
         df.ld_Fidesz = np.random.choice(range(10), len(df))
 
         county_town_digit_groups = (
@@ -136,253 +84,140 @@ def simulate_total_var_dist(df_raw, actual_total_var):
         county_town_digit_sums = county_town_digit_groups.aggregate(
             {"ld_Fidesz": len, "Fidesz": min}
         )
-        county_town_var = (county_town_digit_sums
+        county_town_ent = (county_town_digit_sums
                            .groupby(["Megye", "Telepules"])
                            .aggregate({"ld_Fidesz": total_freq_ent,
                                        "Fidesz": min}))
-        suspects5 = county_town_var.loc[digit_sum_extr.Fidesz["min"] >= MIN_VOTERS_THRESHOLD]
+        suspects5 = county_town_ent.loc[digit_sum_extr.Fidesz["min"] >= MIN_VOTERS_THRESHOLD]
         suspects5 = suspects5.sort_values(["ld_Fidesz"], ascending=[True])
         suspects5 = suspects5.head(N_TOP_WEIRDEST)
 
-        sim_total_var = np.sum(suspects5.ld_Fidesz)
+        sim_total_ent = np.sum(suspects5.ld_Fidesz)
 
-        if actual_total_var < sim_total_var:
+        if sim_total_ent <= actual_total_ent:
             hits += 1
+        entropies.append(sim_total_ent)
 
         if i % 20 == 0:
             try:
                 dt = datetime.now() - start
                 its_per_sec = (i + 1) / dt.total_seconds()
                 print("\r%s elapsed, %d/%d done, %.2f iterations per sec.            " %
-                      (str(dt), i + 1, N_ITERATIONS, its_per_sec),
+                      (str(dt), i + 1, iterations, its_per_sec),
                       end="")
             except:
                 print("\r(failed to calculate iteratons per sec.)              ", end="")
-        # print(sum(suspects3.max_to_mean), np.mean(suspects3.max_to_mean))
     print()  # otblast!
+    print(hits / iterations * 100)
 
-    print(hits / N_ITERATIONS * 100)
-    return hits / N_ITERATIONS
-
-
-# N_TOP_WEIRDEST = 50
-N_TOP_WEIRDEST = 20
-MIN_VOTERS_THRESHOLD = 100
-N_ITERATIONS = 3000
-SEEDS = [1234] #  1234, 1236, 1237, 1238]
+    return hits / iterations, entropies
 
 
-county_town_var = (county_town_digit_sums
-                   .groupby(["Megye", "Telepules"])
-                   .aggregate({"ld_Fidesz": total_freq_ent,
-                               "Fidesz": min}))
-suspects5 = county_town_var.loc[digit_sum_extr.Fidesz["min"] >= MIN_VOTERS_THRESHOLD]
-suspects5 = suspects5.sort_values(["ld_Fidesz"], ascending=[True])
-suspects5 = suspects5.head(N_TOP_WEIRDEST)
+def save_results(actual_total_ent, probabilities, entropies):
+    df = pd.DataFrame(dict(names=["actual_total_ent"],
+                           values=[actual_total_ent]))
+    df.to_csv("app5_actual_total_ent.csv", index=False)
 
-actual_total_var = np.sum(suspects5.ld_Fidesz)
+    df = pd.DataFrame(dict(probability=probabilities))
+    df.to_csv("app5_ent_in_top_probs.csv", index=False)
 
-print("actual data gave", actual_total_var)
-
-
-results = []
-for seed in SEEDS:
-    print("simulating with seed", seed,
-          "voters threshold", MIN_VOTERS_THRESHOLD,
-          "iterations", N_ITERATIONS,
-          ("top %d weirdest (most skewed)" % N_TOP_WEIRDEST))
-    np.random.seed(seed)
-    # just the top 100
-    result = simulate_total_var_dist(
-        df, actual_total_var
-    )
-    print("got probability", result)
-    results.append(result)
+    df = pd.DataFrame(dict(entropy=entropies))
+    df.to_csv("app5_ent_in_top_entropies.csv", index=False)
 
 
-print("results:", results)
+def load_results():
+    df = pd.read_csv("app5_actual_total_ent.csv")
+    actual_total_ent = df["values"][0]
+
+    df = pd.read_csv("app5_ent_in_top_probs.csv")
+    probabilities = df.probability
+
+    df = pd.read_csv("app5_ent_in_top_entropies.csv")
+    entropies = df.entropy
+
+    return actual_total_ent, probabilities, entropies
 
 
-def get_suspects_last_digits(df: pd.DataFrame, suspects: pd.DataFrame):
-    suspects = suspects.reset_index()
-    df = df.reset_index()
-    joined = pd.merge(suspects[["Megye", "Telepules"]],
-                      df[["Megye", "Telepules", "ld_Fidesz"]],
-                      how="inner", on=["Megye", "Telepules"])
-    return joined.ld_Fidesz
+def plot_entropy_distribution():
+    actual_total_ent, probabilities, entropies = load_results()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    h = ax.hist(entropies, bins = 40)
+    ax.axvline(x=actual_total_ent, color="red")
+    _, disp_y = ax.transAxes.transform((0, 1))
+    disp_y -= 20
+    x1, y = ax.transData.inverted().transform((10, disp_y))
+    x2, _ = ax.transData.inverted().transform((0, disp_y))
+    ax.text(actual_total_ent + x1 - x2, y,
+            u"P \u2248 %.2f %%" % (np.mean(probabilities) * 100))
+    plt.show()
 
 
-def test_suspects_full_entropy(df: pd.DataFrame, suspects: pd.DataFrame,
-                               seed=1234, iterations=1000):
-    np.random.seed(seed)
-    ld_suspects5 = get_suspects_last_digits(df, suspects)
-    actual_entropy = entropy(ld_suspects5.value_counts())
-    hits = 0
-    for i in range(iterations):
-        simulated_series = pd.Series(
-            np.random.choice(range(10), len(ld_suspects5))
+if __name__ == "__main__":
+    N_TOP_WEIRDEST = 20
+    MIN_VOTERS_THRESHOLD = 100
+    N_ITERATIONS = 1000
+    SEEDS = [1234, 1235, 1236, 1237]
+
+    county_town_ent = (county_town_digit_sums
+                       .groupby(["Megye", "Telepules"])
+                       .aggregate({"ld_Fidesz": total_freq_ent,
+                                   "Fidesz": min}))
+    suspects5 = county_town_ent.loc[digit_sum_extr.Fidesz["min"] >= MIN_VOTERS_THRESHOLD]
+    suspects5 = suspects5.sort_values(["ld_Fidesz"], ascending=[True])
+    suspects5 = suspects5.head(N_TOP_WEIRDEST)
+
+    actual_total_ent = np.sum(suspects5.ld_Fidesz)
+
+    print("actual data gave", actual_total_ent)
+
+
+    probabilities = []
+    all_entropies = []
+    for seed in SEEDS:
+        print("simulating with seed=", seed,
+              "voters threshold=", MIN_VOTERS_THRESHOLD,
+              "iterations=", N_ITERATIONS,
+              ("; top %d weirdest (most skewed)" % N_TOP_WEIRDEST))
+        np.random.seed(seed)
+        # just the top 100
+        probability, entropies = get_simulated_total_ent_prob(
+            df, actual_total_ent, N_ITERATIONS
         )
-        simulated_entropy = entropy(simulated_series.value_counts())
-        # print("simulated entropy:", simulated_entropy)
-        if actual_entropy >= simulated_entropy:
-            hits += 1
-        if i % 1000 == 999:
-            print(i + 1, "iterations done")
-    print("Probability of getting such a low entropy is %.2f %%" %
-          (hits / iterations * 100, ))
-    return hits / iterations
-
-test_suspects_full_entropy(df, suspects5, iterations = 10000, seed=1235)
-test_suspects_full_entropy(df, suspects5.iloc[:30], iterations = 10000)
-test_suspects_full_entropy(df, suspects5.iloc[:20], iterations = 10000)
-test_suspects_full_entropy(df, suspects5.iloc[:25], iterations = 10000)
-
-# actual data gave 457.50160660601597
-# simulating with seed 1234 voters threshold 100 iterations 200 top 200 weirdest (most skewed)
-# [0.96, 0.95, 0.935, 0.975]
-
-# actual data gave 1148.2771345042297
-# simulating with seed 1234 voters threshold 100 iterations 200 top 500 weirdest (most skewed)
-#
-# [0.96, 0.95, 0.935, 0.975]
-
-# actual data gave 227.24309730661128
-# simulating with seed 1234 voters threshold 100 iterations 200 top 100 weirdest (most skewed)
-#
-# [0.96, 0.95, 0.935, 0.975]
-
-# actual data gave 227.24309730661128
-# simulating with seed 1234 voters threshold 100 iterations 1000 top 100 weirdest (most skewed)
-# [0.939, 0.955, 0.947, 0.944, 0.934]
-
-# actual data gave 112.11384265690897
-# simulating with seed 1234 voters threshold 100 iterations 200 top 50 weirdest (most skewed)
-# [0.96, 0.95, 0.935, 0.975, 0.955]
-
-# actual data gave 21.408063409481382
-# simulating with seed 1234 voters threshold 100 iterations 200 top 10 weirdest (most skewed)
-# [0.935, 0.945, 0.955, 0.985, 0.94]
-
-# 1) I should multiprocess it - okay, reproducibility's with me, who's against?
-# 2)
+        probabilities.append(probability)
+        all_entropies.extend(entropies)
 
 
-# it's probably replicating the "weird event in simulated data" playbook
-# so I've got two choices now: more iterations, more seeds, fewer "top"
-# I choose
+    print("Simulations were carried out with seeds",
+          ",".join([str(seed) for seed in SEEDS])),
+    print("Resulting probabilities of such a low overall entropy:",
+          ", ".join(["%.2f %%" % (p * 100) for p in probabilities]))
+    print("Mean probability: %.2f %%" % (100 * np.mean(probabilities)))
+
+    save_results(actual_total_ent, probabilities, all_entropies)
+
+    plot_entropy_distribution()
 
 
 """
 Wisdom of the day:
-1) the common definition of entropy is infeasible for short sequneces
+1) the common definition of entropy is infeasible for short sequences
 the emp. frequencies (for two symbols)
 (2 1)
 can both suggest that this is a uniform or a 2:1 distribution
 (or something else, for the matter, primarily in the range)
 
-however it is treated as a definite 2:1 probability thing
+However it is treated as a definite thing.
 
-if I were to take each configuration a symbol, generate
+If I were to take each configuration as a symbol, generate
 and assign a probability to each, that would mean I could
 calculate a joint probability of all of them appearing
-together
+together.
 
-Why would I calculate an anything though?
-
-JUST give a damn.
-
-Well I could calculate county-wide IE too.
-
-
-
-
-concatenated data frame has 10278 rows
-actual data gave 21.408063409481382
-simulating with seed 1235 voters threshold 100 iterations 4000 top 10 weirdest (most skewed)
-
-1:01:56.731875 elapsed, 1.07 iterations per sec.
-93.65
-got probability 0.9365
-
-
-concatenated data frame has 10278 rows
-actual data gave 21.408063409481382
-simulating with seed 1234 voters threshold 100 iterations 4000 top 10 weirdest (most skewed)
-
-1:06:55.168365 elapsed, 0.99 iterations per sec.
-94.0
-got probability 0.94
-
-
-
-
-Top 50, 4000 iterations
-
-concatenated data frame has 10278 rows
-actual data gave 112.11384265690897
-simulating with seed 1235 voters threshold 100 iterations 4000 top 50 weirdest (most skewed)
-
-1:02:14.988104 elapsed, 1.07 iterations per sec.
-94.05
-got probability 0.9405
-
-concatenated data frame has 10278 rows
-actual data gave 112.11384265690897
-simulating with seed 1234 voters threshold 100 iterations 4000 top 50 weirdest (most skewed)
-
-1:06:58.076387 elapsed, 0.99 iterations per sec.
-94.15
-got probability 0.9415
-
-
-
-actual data gave 43.43930621517053
-simulating with seed 1235 voters threshold 100 iterations 100 top 20 weirdest (most skewed)
-
-0:01:18.258150 elapsed, 81/100 done, 1.04 iterations per sec.
-99.0
-got probability 0.99
-
-
-
-actual data gave 43.43930621517053
-simulating with seed 1235 voters threshold 100 iterations 1000 top 20 weirdest (most skewed)
-
-0:18:40.060501 elapsed, 981/1000 done, 0.88 iterations per sec.
-98.0
-got probability 0.98
-
-
-actual data gave 43.43930621517053
-simulating with seed 1234 voters threshold 100 iterations 1000 top 20 weirdest (most skewed)
-
-0:20:30.986530 elapsed, 981/1000 done, 0.80 iterations per sec.
-97.89999999999999
-got probability 0.979
-results: [0.979]
-
-
-actual data gave 43.43930621517053
-simulating with seed 1234 voters threshold 100 iterations 3000 top 20 weirdest (most skewed)
-
-0:41:25.566769 elapsed, 2981/3000 done, 1.20 iterations per sec.
-97.73333333333333
-got probability 0.9773333333333334
-results: [0.9773333333333334]
-
-
-with 3000 iterations - simple and quite significant - that is what we like!
-vs. a full blown simulation
-
-
-those digits are rather suspicious
-
-
-Next: plot up the likeliness by top N with say 20 iterations
-So as to find the sweetspot
-
-
-The top 20 suspects giving a difficult to beat (3%) entropy
+Anyway.
+The top 20 suspects giving a difficult to "cherry-pick"
+from random data (3% probability of happening by chance)
+entropy:
 
                                          ld_Fidesz  Fidesz
 Megye             Telepules
@@ -408,4 +243,3 @@ VAS               Szombathely              2.220608   145.0
 BUDAPEST          Budapest II. kerület     2.228551   135.0
 
 """
-
