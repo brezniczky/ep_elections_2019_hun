10a34f40ddbb949154b93c73d73320a7f09994d9
Janos Brezniczky <brezniczky@gmail.com>
2019-06-25 19:14:36 +0100

    Processed 2014 data added to the comparative data frame (app6)

--- app6_comparative.py
+++ app6_comparative.py
@@ -2,6 +2,7 @@ from preprocessing import get_cleaned_data
 from digit_entropy_distribution import get_entropy, prob_of_entr
 from collections import OrderedDict
 import pandas as pd
+from AndrasKalman.load import load_2014
 
 
 if not "df" in globals():
@@ -13,6 +14,8 @@ def save_results(
         suspects,
         df_comparative_sorted_by_both,
         suspects2,
+        df_comparative_sorted_by_all_3,
+        suspects3
     ):
     df_comparative.to_csv("app6_comparative_result.csv", index=False)
     suspects.to_csv("app6_suspects.csv", index=False)
@@ -21,6 +24,11 @@ def save_results(
         index=False
     )
     suspects2.to_csv("app6_suspects2.csv", index=False)
+    df_comparative_sorted_by_all_3.to_csv(
+        "app6_comparative_sorted_by_all_3.csv",
+        index=False
+    )
+    suspects3.to_csv("app6_suspects3.csv", index=False)
 
 
 def load_results():
@@ -33,9 +41,12 @@ def load_results():
     df_comparative_sorted_by_both: comparative data sorted by
         p_both_incorr ie. both being incorrect, descending
     suspects2: top suspects based on p_both_incorr
+    df_comparative_sorted_by_all_3: comparative data sorted by
+        p_all_3 i.e. all 3 (2014, 2018, 2019) being regular
+    suspects3: top candidates from df_comparative_sorted_by_all_3.
 
-    Other criteria: at least 8 electoral wards and at least 100
-        Fidesz party voters in each ward in each suspect settlement
+    Other criteria: at least 8 electoral wards and at least 100 Fidesz
+        party votes (in 2019) in each ward in each suspect settlement
         in order to be considered,
     """
     df_comparative = pd.read_csv("app6_comparative_result.csv")
@@ -44,12 +55,40 @@ def load_results():
         "app6_comparative_result_sort_by_both_incorr.csv"
     )
     suspects2 = pd.read_csv("app6_suspects2.csv")
+
+    df_comparative_sorted_by_all_3 = pd.read_csv(
+        "app6_comparative_sorted_by_all_3.csv"
+    )
+    suspects3 = pd.read_csv("app6_suspects3.csv")
+
     return (
         df_comparative,
         suspects,
         df_comparative_sorted_by_both,
-        suspects2
+        suspects2,
+        df_comparative_sorted_by_all_3,
+        suspects3,
     )
+
+
+def calc_prob(row):
+    return prob_of_entr(row["count"], row["ld_entropy"])
+
+
+def load_2014_data():
+    agg_cols = OrderedDict([("ld_Fidesz", [get_entropy, len]),
+                            ("Fidesz", min)])
+
+    df_2014 = load_2014()
+    df_2014["Fidesz"] = df_2014["Fidesz"].astype(int)
+    df_2014["ld_Fidesz"] = df_2014["Fidesz"] % 10
+    df_Fidesz_ent_2014 = df_2014.groupby(["Telepules"]).aggregate(agg_cols)
+    df_Fidesz_ent_2014.reset_index(inplace=True)
+    df_Fidesz_ent_2014.columns = ["Settlement", "ld_entropy", "count", "min"]
+    df_Fidesz_ent_2014["count"] = df_Fidesz_ent_2014["count"].astype(int)
+    df_Fidesz_ent_2014["prob_of_entr"] = df_Fidesz_ent_2014.apply(calc_prob, axis=1)
+
+    return df_Fidesz_ent_2014
 
 
 def generate_data():
@@ -59,9 +98,6 @@ def generate_data():
     df_Fidesz_ent = df.groupby(["Telepules"]).aggregate(agg_cols)
     df_Fidesz_ent.reset_index(inplace=True)
     df_Fidesz_ent.columns = ["Settlement", "ld_entropy", "count", "min"]
-
-    def calc_prob(row):
-        return prob_of_entr(row["count"], row["ld_entropy"])
 
     """
     So two things cab be of interest:
@@ -75,22 +111,36 @@ def generate_data():
     df_Fidesz_ent.sort_values(["prob_of_entr"], inplace=True)
 
     df_Fidesz_ent_2018 = pd.read_csv("Fidesz_entr_prob_2018.csv")
+    df_Fidesz_ent_2014 = load_2014_data()
+
 
     df_comparative = pd.merge(df_Fidesz_ent, df_Fidesz_ent_2018,
                               how="inner", on=["Settlement"])
+    df_Fidesz_ent_2014.columns = [
+        column + "_z" if column != "Settlement" else column
+        for column in df_Fidesz_ent_2014.columns
+    ]
+    df_comparative = pd.merge(df_comparative, df_Fidesz_ent_2014,
+                              how="inner", on=["Settlement"], suffixes=["", "_z"])
+
     df_comparative.columns = ["Settlement", "ld_Entropy_2019", "count_2019",
                               "min_votes_2019", "p_2019",
                               "ld_entropy_2018", "min_votes_2018",
-                              "count_2018", "p_2018"]
-    df_comparative["p_both"] = df_comparative.p_2019 * df_comparative.p_2018
+                              "count_2018", "p_2018",
+                              "ld_Entropy_2014", "count_2014",
+                              "min_votes_2014", "p_2014"]
+
+    df_comparative["p_all_3"] = \
+        df_comparative.p_2019 * df_comparative.p_2018 * df_comparative.p_2014
+    df_comparative["p_all_2"] = df_comparative.p_2019 * df_comparative.p_2018
     df_comparative["p_both_incorr"] = \
         (1 - df_comparative.p_2019) * (1 - df_comparative.p_2018)
-    df_comparative.sort_values(["p_both"], inplace=True)
+    df_comparative.sort_values(["p_all_2"], inplace=True)
 
     # p_both: prob. of both being 'regular'
     # p_both_incorr: prob. of both being 'irregular'
 
-    suspects = df_comparative.loc[(df_comparative.p_both < 0.1) &
+    suspects = df_comparative.loc[(df_comparative.p_all_2 < 0.1) &
                                   (df_comparative.min_votes_2019 >= 100 ) &
                                   (df_comparative.count_2018 >= 8)]
     df_comparative_sorted_by_both = \
@@ -100,8 +150,17 @@ def generate_data():
         (df_comparative_sorted_by_both.min_votes_2019 >= 100) &
         (df_comparative_sorted_by_both.count_2018 >= 8)
     ]
+
+    df_comparative_sorted_by_all_3 = df_comparative.sort_values(["p_all_3"])
+    suspects3 = df_comparative_sorted_by_all_3.loc[
+        (df_comparative_sorted_by_all_3.p_all_3 <= 0.1) &
+        (df_comparative_sorted_by_both.min_votes_2019 >= 100) &
+        (df_comparative_sorted_by_both.count_2018 >= 8)
+    ]
+
     save_results(df_comparative, suspects,
-                 df_comparative_sorted_by_both, suspects2)
+                 df_comparative_sorted_by_both, suspects2,
+                 df_comparative_sorted_by_all_3, suspects3)
 
 
 if __name__ == "__main__":
