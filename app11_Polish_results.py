1bd66366064fd60480bd8eb62e40456f05fe2d4d
Janos Brezniczky <brezniczky@gmail.com>
2019-07-15 15:11:51 +0100

    Polish results

---
+++ app11_Polish_results.py
@@ -0,0 +1,349 @@
+"""
+Disclaimer: the author is not really a statistician.
+He is aware of potentially making a complete fool out of
+myself err.. never mind the syntax :)
+
+However, as long as nobody's apparently dealing with these,
+we half-layman might as well try to give a hand.
+"""
+import pandas as pd
+import os.path
+import scipy.stats as st
+import numpy as np
+from digit_entropy_distribution import get_entropy, LodigeTest
+import matplotlib.pyplot as plt
+from collections import OrderedDict, defaultdict
+from functools import lru_cache
+
+
+cols = [
+    "Komisja otrzymała kart do głosowania",
+    "Nie wykorzystano kart do głosowania",
+    "Lista nr 3 - KKW KOALICJA EUROPEJSKA PO PSL SLD .N ZIELONI",
+    "1 LEWANDOWSKI Janusz Antoni",
+    "2 ADAMOWICZ Magdalena",
+    "Lista nr 4 - KW PRAWO I SPRAWIEDLIWOŚĆ",
+    "1 FOTYGA Anna Elżbieta",
+    "2 SELLIN Jarosław Daniel",
+]
+total_col = "Komisja otrzymała kart do głosowania"
+min_vote_count = 100
+entropy_window_size = 10
+
+
+def get_cleaned_data():
+    csv_filename = "PL/wyniki_gl_na_kand_po_obwodach_sheet_1.csv"
+    if os.path.exists(csv_filename):
+        try:
+            df = pd.read_csv(csv_filename)
+            return df
+        except Exception as e:
+            print("Error reading saved cleaned data, attempting to recreate ...")
+            print("  details:", str(e))
+    else:
+        print("Cleaning data...")
+
+    xlsx_filename = "PL/wyniki_gl_na_kand_po_obwodach.xlsx"
+    xls = pd.ExcelFile(xlsx_filename)
+    sheet_name = xls.sheet_names[0]
+    print("Reading sheet", sheet_name)
+    df = pd.read_excel(xlsx_filename, sheet_name=sheet_name)
+    df.to_csv(csv_filename, index=False)   # , encoding="utf-8")
+    return df
+
+
+@lru_cache(maxsize=1)
+def get_xlsx(filename):
+    return pd.ExcelFile(filename)
+
+
+def get_big_cleaned_data():
+    sheet_list_csv_filename = "PL/wyniki_gl_na_kand_po_obwodach_sheet_list.csv"
+    sheet_csv_filename = "PL/wyniki_gl_na_kand_po_obwodach_sheet_%s.csv"
+    if os.path.exists(sheet_list_csv_filename):
+        try:
+            df_sheets = pd.read_csv(sheet_list_csv_filename, encoding="utf-8")
+            dfs = []
+            for sheet_id in df_sheets.id:
+                dfs.append(pd.read_csv(sheet_csv_filename % sheet_id))
+            return dfs
+        except Exception as e:
+            print("Error reading saved cleaned data, attempting to recreate ...")
+            print("  details:", str(e))
+
+    print("Cleaning data...")
+
+    xlsx_filename = "PL/wyniki_gl_na_kand_po_obwodach.xlsx"
+    xls = get_xlsx(xlsx_filename)
+
+    """ I can just save it as a bunch of CSV's and worry later rapidly
+        about the elegant format depending on the scenario """
+    dfs = []
+    sheet_ids = []
+    sheet_names = []
+    id = 0
+    for sheet_name in xls.sheet_names:
+        print("Reading sheet", sheet_name)
+        df = pd.read_excel(xlsx_filename, sheet_name=sheet_name)
+        id += 1
+        df.to_csv(sheet_csv_filename % id, index=False, encoding="utf-8")
+        sheet_ids.append(id)
+        sheet_names.append(sheet_name)
+        dfs.append(df)
+
+    df_sheets = pd.DataFrame(dict(id=sheet_ids, sheet_names=sheet_names))
+    df_sheets.to_csv(sheet_list_csv_filename, index=False, encoding="utf-8")
+
+
+def check_column_code_stats(df, silent=False):
+    codes = df[[df.columns[0]]]
+    codes.columns = ["area_code"]
+    stats = (
+        codes.groupby(["area_code"])
+        .aggregate({"area_code": len})
+        .rename(columns={"area_code": "area_code_count"})
+        .reset_index()
+    )
+    if not silent:
+        print("codes frequencies:")
+        for i in range(len(stats)):
+            row = stats.iloc[i]
+            print("  ", row[0], row[1])
+    return stats
+
+def get_feasible_subseries(arr, min_votes):
+    arr = arr[arr >= (min_votes - 5) + np.random.choice(range(10), len(arr))]
+    return arr
+
+
+def check_digit_doubling(df):
+    df = df[cols]
+
+    np.random.seed(1234)
+
+    for col in cols:
+        nrs = df[col].values
+        nrs = get_feasible_subseries(nrs, 100)
+        digs = nrs % 10
+        # 2nd two digits are the same
+        dbls = sum(((nrs % 100) % 11) == 0)
+        print(col)
+        print("  nr. of suitable numbers", len(digs))
+        dup_cnt = sum(digs[1:] == digs[:-1])
+        dup_freq = dup_cnt / (len(digs) - 1)
+        print("  digit replication rel. freq.:", dup_freq, )
+        p = 1 - st.poisson((len(digs) -- 1) / 10).cdf(dup_cnt)
+        print("  probability: %.2f %%" % (p * 100))
+        print("  two last digits are equal rel. freq.:", dbls / len(digs))
+        print()
+
+
+def get_windowed_entropy(series, window_size=entropy_window_size):
+    ent = 0
+    for i in range(len(series) - window_size + 1):
+        ent += get_entropy(series[i:(i + window_size)])
+    return ent
+
+
+def check_windowed_entropies(df):
+    """ this unfortunately visibly yields too much of variance in the
+        statistic to construct a confident result
+
+        the plan was to generally reflect on a - to the eye, apparent -
+        memory effect in the last digit sequences.
+
+        not sensitive enough (or my eyes were too eager to find
+        anything) - may need some filtering etc. not going deeply into
+        that at this point
+    """
+    def print_series(col, nrs):
+        nrs = get_feasible_subseries(nrs, 50)
+        digs = nrs % 10
+        ent = get_windowed_entropy(digs, window_size=entropy_window_size)
+        ent_per_window = ent / (len(digs) - entropy_window_size + 1)
+
+        print(col)
+        print("  series len:", len(digs))
+        print("  entropy:", ent)
+        print("  entropy per window:", ent_per_window)
+
+    df = df[cols]
+    for col in cols:
+        print_series(col, df[col].values)
+    print_series("ideal", 100 + np.random.choice(range(10), len(df)))
+
+
+def get_feasible_areas(df, vote_cols, min_n_wards, min_votes):
+    # The min_votes function is effectively unused
+    area_col = df.columns[0]
+    agg_cols = [(area_col, len)] + [(col, min) for col in vote_cols]
+    res_colnames = ["n_wards"] + ["min_" + col for col in vote_cols]
+    agg = (
+        df[[area_col] + list(vote_cols)]
+        .groupby([area_col])
+        .aggregate(OrderedDict(agg_cols))
+    )
+    agg.columns = res_colnames
+    agg = agg.reset_index()
+    cond = agg.n_wards >= min_n_wards
+    for col in vote_cols:
+        cond = cond & (agg["min_" + col] >= min_votes)
+    return agg.loc[cond][area_col]
+
+
+def check_likelihoods(df, cols, self_test=False):
+    """
+    Area based filtering by vote is not feasible here - few areas (if any,
+    experience suggests none) have votes only above 100 for multiple lists,
+    for instance.
+    """
+    feasible_areas = get_feasible_areas(df, cols, 0, 0)   # 0, 0
+    area_col = df.columns[0]
+    df = df.loc[df[area_col].isin(feasible_areas)]
+
+    # hence, the df is filtered by min votes on individual row basis
+    def row_min_is_ok(row):
+        return min([row[col] for col in cols]) >= 101    #  25
+    is_ok = df.apply(row_min_is_ok, axis=1)
+    df = df.loc[is_ok].copy()
+
+    if self_test:
+        for col in cols:
+            df[col] = np.random.choice(list(range(100, 110)), len(df))
+
+    tests = {}
+    for col in cols:
+        print("Column:", col)
+        test = LodigeTest(
+            df[col].values % 10,
+            df[df.columns[0]].values,
+            bottom_n=20,
+            pe_iterations=50000,
+        )
+        print("  actual likelihood:", test.likelihood)
+        print("  p-value:", test.p)
+        tests[col] = test
+
+    return tests
+
+
+def plot_test_p_value_hists(tests: dict):
+    """ I found these plots pretty unhelpful, not that I'm never wrong ;)
+        Anyhow, please welcome the ...
+    """
+    for col, test in tests.items():
+        plt.hist(act_p_values.values(), bins=20)
+        plt.title(col)
+        plt.show()
+
+
+def calc_p_values_by_area(tests: dict) -> pd.DataFrame:
+    # experimental stuff: likeliness by area code
+    prod_p_values = defaultdict(lambda: 1)
+    for col, test in tests.items():
+        act_p_values = tests[col].p_values
+        for group_id, p in act_p_values.items():
+            prod_p_values[group_id] *= p
+
+    def fmt(key):
+        # forgot to cast to str -- int -> area code padding here
+        return "%.6d" % key
+
+    order = sorted([fmt(key) for key in prod_p_values.keys()])
+    df = pd.DataFrame(dict(
+        area_code=order,
+        p_value=[prod_p_values[int(area_code)] for area_code in order],
+    ))
+    return df
+
+
+def merge_lista_results(dfs, lista_idxs_to_exclude):
+    print("merging \"lista\" columns...")
+    merged = []
+    lista_cols = set()
+
+    for df in dfs:
+        for col in df.columns:
+            if col.startswith("Lista nr"):
+                lista_cols.add(col)
+
+    def get_lista_col_idx(col):
+        return int(col.split(" ")[2])
+
+    lista_cols = sorted(lista_cols, key=get_lista_col_idx)
+    lista_cols = [col for col in lista_cols
+                  if get_lista_col_idx(col)
+                  not in lista_idxs_to_exclude]
+    cols_to_keep = ["Kod terytorialny gminy"] + lista_cols
+
+    dfs_to_merge = []
+    for df in dfs:
+        cols_dict = OrderedDict([
+            (col_name,
+             df[col_name]
+             if col_name in df.columns else None)
+            for col_name in cols_to_keep
+        ])
+        dfs_to_merge.append(pd.DataFrame(cols_dict))
+    merged = pd.concat(dfs_to_merge)
+
+    return merged
+
+
+if __name__ == "__main__":
+    # cols = [
+    #     "Lista nr 3 - KKW KOALICJA EUROPEJSKA PO PSL SLD .N ZIELONI",
+    #     "1 LEWANDOWSKI Janusz Antoni",
+    #     "Lista nr 4 - KW PRAWO I SPRAWIEDLIWOŚĆ",
+    #     "1 FOTYGA Anna Elżbieta",
+    # ]
+    # df = get_cleaned_data()
+    # # check_windowed_entropies(df)
+    # # check_digit_doubling(df)
+    # # print("Self test (should provide probabilities far from zero)")
+    # # check_likelihoods(df, self_test=True)
+    # print("Real test")
+    # check_likelihoods(df)
+    dfs = get_big_cleaned_data()
+    merged = merge_lista_results(
+        dfs,
+        lista_idxs_to_exclude=[8, 9, 10]
+    )
+    # it is impractical to check on numerous columns, as this will
+    # reduce the data set - a constraint for keeping rows is that
+    # each column features a big enough value. now imagine that this
+    # is a big "and" of constraints, any of them being too small drops
+    # the whole row.
+    # this test was unsuccessful at uncovering much, but possibly
+    # due to lack of data.
+
+    # check_likelihoods(merged, merged.columns[1:])
+
+    # the primary aim is to deal with the two biggest winners
+    tests = check_likelihoods(merged, merged.columns[3:5])
+    plot_test_p_value_hists(tests)
+    p_values = calc_p_values_by_area(tests).sort_values(["p_value"])
+
+    print("Area codes with least unlikely correct results:")
+    print(p_values.head(20))
+
+    p_values.to_csv("app11_top_suspects.csv")
+
+    print("THE BELOW IS WRONG!!! but could useful as a concept, "
+          "though doesn't look very stable. due to the reciprocal.")
+    print("A rough estimate says these events should have occurred out of")
+    print("an est. %d cases - there are %d cases." %
+          (int(round(sum(1 / p_values.p_value))), len(p_values)))
+
+
+
+"""
+In [213]: sum(1 / p_values.p_value)
+Out[213]: 6133.625219317689
+
+In [214]: len(p_values)
+Out[214]: 282
+
+"""
+    # .. and what we get is that there are likely "inaccuracies" around
+    # lista nr. 4's results.
--- digit_entropy_distribution.py
+++ digit_entropy_distribution.py
@@ -1,6 +1,7 @@
 from scipy.stats import entropy, poisson
 import numpy as np
 from functools import lru_cache
+import pandas as pd
 
 
 _DEFAULT_SEED = 1234
@@ -8,10 +9,10 @@ _DEFAULT_ITERATIONS = 10000
 
 
 _DEFAULT_BOTTOM_N = 20
-_DEFAULT_RANDOM_SEED = 1234
-_DEFAULT_ITERATIONS = 10
+_DEFAULT_LL_SEED = 1234
+_DEFAULT_LL_ITERATIONS = 1000
 _DEFAULT_PE_RANDOM_SEED = 1234
-_DEFAULT_PE_ITERATIONS = 1234  # 12345
+_DEFAULT_PE_ITERATIONS = 12345
 
 
 def get_entropy(x):
@@ -55,7 +56,8 @@ def prob_of_twins(x):
 
 def generate_sample(n_wards,
                     seed=_DEFAULT_SEED,
-                    iterations=_DEFAULT_ITERATIONS):
+                    iterations=_DEFAULT_ITERATIONS,
+                    quiet=False):
     np.random.seed(seed)
     entrs = []
     for i in range(iterations):
@@ -63,38 +65,45 @@ def generate_sample(n_wards,
         _, counts = np.unique(values, return_counts=True)
         entr = entropy(counts / sum(counts))
         entrs.append(entr)
-    print("cdf for %d was generated" % n_wards)
+    if not quiet:
+        print("cdf for %d was generated" % n_wards)
     return np.array(entrs)
 
 
 @lru_cache(maxsize=1000)
 def get_cdf_fun(n_wards,
                 seed=_DEFAULT_SEED,
-                iterations=_DEFAULT_ITERATIONS):
+                iterations=_DEFAULT_ITERATIONS,
+                quiet=False):
     """ Return a 'forgiving' CDF, that is, one where
         equality allowed: F where
         F(y) = P(X <= y) """
-    sample = generate_sample(n_wards, seed, iterations)
+    sample = generate_sample(n_wards, seed, iterations, quiet=quiet)
     values, counts = np.unique(sample, return_counts=True)
     total = sum(counts)
     counts = np.cumsum(counts)
 
-    def cdf_fun(y):
+    def cdf_fun(y, avoid_inf=False):
         idx = np.digitize(y, values) - 1
         if idx >= 0:
             return counts[idx] / total
-        else:
+        elif not avoid_inf:
             return 0
+        else:
+            # give a probability that likely goes undetected
+            return 1 / total
 
     return cdf_fun
 
 
 def prob_of_entr(n_wards, entr,
                  seed=_DEFAULT_SEED,
-                 iterations=_DEFAULT_ITERATIONS):
-    """ probability of the entropy being this small """
-    cdf = get_cdf_fun(n_wards, seed, iterations)
-    return cdf(entr)
+                 iterations=_DEFAULT_ITERATIONS,
+                 avoid_inf=False,
+                 quiet=False):
+    """ Probability of the entropy being this small. """
+    cdf = get_cdf_fun(n_wards, seed, iterations, quiet)
+    return cdf(entr, avoid_inf=avoid_inf)
 
 
 """
@@ -102,42 +111,66 @@ Log likelihood assessment of groups of digits of varying size for uniformity.
 
 Uses simulations, slow.
 
-Relies on non-parametric CDFs.
+Relies on non-parametric CDFs generated above.
 """
 def get_log_likelihood(digits, slice_limits, bottom_n,
-                       seed, iterations, towns=None):
+                       seed, iterations, towns=None, return_probs=False,
+                       avoid_inf=False, quiet=False):
     slices = [digits[a:b] for a, b in slice_limits]
     entropies = [get_entropy(s) for s in slices]
-    probs = [prob_of_entr(len(s), e, seed, iterations)
-            for s, e in zip(slices, entropies)]
+    probs = [prob_of_entr(len(s), e, seed, iterations,
+                          avoid_inf=avoid_inf, quiet=quiet)
+             for s, e in zip(slices, entropies)]
     bottom_probs = sorted(probs)[:bottom_n]
-    if towns is not None:
+    # TODO: remove the towns param that even I can't remember ;)
+    if towns is not None and not quiet:
         print(towns[np.array(probs) <= max(bottom_probs)])
     l = sum(np.log(bottom_probs))
-    return l
+    if not return_probs:
+        return l
+    else:
+        return l, probs
 
 
 def get_likelihood_cdf(slice_limits, bottom_n,
-                       seed=_DEFAULT_RANDOM_SEED,
-                       iterations=_DEFAULT_ITERATIONS,  # voting simulation
-                       pe_seed=_DEFAULT_RANDOM_SEED,
-                       pe_iterations=_DEFAULT_ITERATIONS):  # entropy
+                       seed=_DEFAULT_LL_SEED,
+                       iterations=_DEFAULT_LL_ITERATIONS,  # voting simulation
+                       pe_seed=_DEFAULT_PE_RANDOM_SEED,
+                       pe_iterations=_DEFAULT_PE_ITERATIONS,
+                       quiet=False):  # entropy
 
     sample = []
     # end of the last slice is the ...
     n_settlements = slice_limits[-1][1]
     warnings = 0
 
+    """
+    To allow the other function (ugly as hell) generate CDFs using
+    its own random seed control, and later have exclusive control
+    over the seed, ensure the CDFs are ready ahead of the iterations.
+    (Temp. fix - should have different random sequences.)
+    """
+    np.random.seed(seed)
+    for i in range(min(iterations, 1)):
+        digits = np.random.choice(range(10), n_settlements)
+        sim_likelihood = get_log_likelihood(digits, slice_limits,
+                                            bottom_n, pe_seed, pe_iterations,
+                                            quiet=quiet)
+
+    """ Now carry out the actual calculations... """
     np.random.seed(seed)
     for i in range(iterations):
         digits = np.random.choice(range(10), n_settlements)
         sim_likelihood = get_log_likelihood(digits, slice_limits,
-                                            bottom_n, pe_seed, pe_iterations)
+                                            bottom_n, pe_seed, pe_iterations,
+                                            quiet=quiet)
         sample.append(sim_likelihood)
-        if np.isinf(sim_likelihood):
-            print("Warning! Infinite simulated likelihood - perhaps increase the p.e. iterations!")
+        # -Inf will count in a "forgiving" way still, so a warning only
+        if np.isinf(sim_likelihood) and not quiet:
+            print("Warning! Infinite simulated likelihood encountered - "
+                  "perhaps increase the p.e. iterations?")
             warnings += 1
-        if i % 50 == 0:
+        if i % 50 == 0 and not quiet:
             print(i, np.mean(np.array(sample)[~np.isinf(sample)]))
 
     values, counts = np.unique(sample, return_counts=True)
@@ -145,7 +178,7 @@ def get_likelihood_cdf(slice_limits, bottom_n,
     counts = np.cumsum(counts)
 
     def cdf(l):
-        """ Forgiving CDF: P(L <= L_actual) """
+        """ CDF: P(L <= L_actual) """
         idx = np.digitize(l, values) - 1
         if idx >= 0:
             return counts[idx] / total
@@ -160,6 +193,151 @@ def get_likelihood_cdf(slice_limits, bottom_n,
     return cdf
 
 
+class LogLikelihoodDigitGroupEntropyTest():
+
+    @staticmethod
+    def get_slice_limits(group_ids):
+        assert all(group_ids == sorted(group_ids)), \
+               "Settlement values must be in a sorted order!"
+        values = np.unique(group_ids)
+        settlement_index = {
+            name: i for name, i in zip(values, range(len(values)))
+        }
+        indexes = pd.Series([settlement_index[g] for g in group_ids])
+        prev_indexes = pd.Series([-1] + list(indexes[:-1]))
+        next_indexes = pd.Series(list(indexes[1:]) + [len(indexes)])
+        starts = np.where(indexes != prev_indexes)[0]
+        ends = np.where(indexes != next_indexes)[0] + 1
+        return list(zip(starts, ends))
+
+    def __init__(self,
+                 digits, group_ids, bottom_n,
+                 # log likelihood probability calc. hyperparam.
+                 ll_iterations=_DEFAULT_LL_ITERATIONS,
+                 ll_seed=_DEFAULT_LL_SEED,
+                 # probability of entropy hyperparam.
+                 pe_iterations=_DEFAULT_PE_ITERATIONS,
+                 pe_seed=_DEFAULT_SEED,
+                 quiet=False):
+
+        # avoid index keys interfering with the intentions
+        if type(digits) is pd.Series:
+            digits = digits.values
+        if type(group_ids) is pd.Series:
+            group_ids = group_ids.values
+
+        # TODO: should I carry the sorting out here? yes
+        df = pd.DataFrame({
+            "group_id": group_ids,
+            "digit": digits,
+        })
+        df.sort_values(["group_id"], inplace=True)
+        self._digits = df["digit"].values
+        self._group_ids = df["group_id"].values
+        self._slice_limits = self.get_slice_limits(self._group_ids)
+
+        self._bottom_n = bottom_n
+        self._ll_iterations = ll_iterations
+        self._ll_seed = ll_seed
+        self._pe_iterations = pe_iterations
+        self._pe_seed = pe_seed
+
+        self._likelihood = None
+        self._p_values = None
+        self._cdf = None
+        self._quiet = quiet
+
+    def _init_likelihood_and_probs(self):
+        self._likelihood, self._p_values = get_log_likelihood(
+            self._digits, self._slice_limits,
+            self._bottom_n,
+            self._pe_seed, self._pe_iterations,
+            return_probs=True,
+            # obtain an "upper estimate" on the likelihood to
+            # avoid possible Infs resulting from numeric resolution
+            avoid_inf=True,
+            quiet=self._quiet
+        )
+
+    @property
+    def likelihood(self):
+        if self._likelihood is None:
+            self._init_likelihood_and_probs()
+        return self._likelihood
+
+    @property
+    def cdf(self):
+
+        if self._cdf is None:
+            self._cdf = get_likelihood_cdf(
+                self._slice_limits, self._bottom_n,
+                self._ll_seed, self._ll_iterations,
+                self._pe_seed, self._pe_iterations,
+                quiet=self._quiet
+            )
+        return self._cdf  # TODO: that is not exactly read-only...
+
+    @property
+    def p(self):
+        return self.cdf(self.likelihood)
+
+    @property
+    def p_values(self):
+        if self._p_values is None:
+            self._init_likelihood_and_probs()
+        return dict(
+            list(zip(self._group_ids, self._p_values))
+        )
+
+
+""" Short name for now... """
+LodigeTest = LogLikelihoodDigitGroupEntropyTest
+
+
 if __name__ == "__main__":
-    print(prob_of_entr(47, 2.1485))
+    # print(prob_of_entr(47, 2.1485))
     # expect ~ 0.133 (got with seed=1234) depending on the seed
+    test = LodigeTest(
+        [1] * 8 + [2] * 4, [1] * 12, 10
+    )
+    print(test.p)
+    test = LodigeTest(
+        list(range(10)), [10] * 10, 10
+    )
+    print(test.p)
+    test = LodigeTest(
+        list(range(5)) + [1] * 5, [10] * 10, 10
+    )
+    print(test.p)
+
+
+"""
+In [117]:
+f2 = get_cdf_fun(200)
+ys2 = np.array([f2(x) for x in xs])
+xs2 = xs[ys2 < 0.5]
+ys2 = ys2[ys2 < 0.5]
+plt.plot(xs2, np.log(ys2))
+plt.show()
+
+f2 = get_cdf_fun(100)
+ys2 = np.array([f2(x) for x in xs])
+xs2 = xs[ys2 < 0.5]
+ys2 = ys2[ys2 < 0.5]
+plt.plot(xs2, np.log(ys2))
+plt.show()
+
+f2 = get_cdf_fun(500, iterations=50000)
+ys2 = np.array([f2(x) for x in xs])
+xs2 = xs[ys2 < 0.5]
+ys2 = ys2[ys2 < 0.5]
+plt.plot(xs2, np.log(ys2))
+plt.show()
+
+
+TODO: left tail apparently (and as conjectured from
+early stage analytical work) could be approximated by
+an exponential form.
+Then we could estimate probability of outlying
+extremities based on that.
+"""
