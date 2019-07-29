282ee05c91db3f6c1e1190bb43100dd92872da4b
Janos Brezniczky <brezniczky@gmail.com>
2019-07-29 20:20:20 +0100

    Vote dist. by digit: vs. mean chart option added

--- digit_distribution_charts.py
+++ digit_distribution_charts.py
@@ -2,9 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+_USE_MEAN_DIGIT_GROUP = 4.5
+
+
 def _plot_votes_of_digits_hist(df, party, digit_groups, n_bins=20,
                                max_votes=None, title=None,
                                last_digit_colname_prefix="ld_"):
+
+    """ Special digit group value: 4.5, meaning "mean of all". """
+
     if max_votes is None:
         max_votes = max(df[party])
     bin_size = max_votes / n_bins
@@ -15,12 +21,43 @@ def _plot_votes_of_digits_hist(df, party, digit_groups, n_bins=20,
     bins = np.arange(0, max_votes, bin_size)
     alpha = 1 / len(digit_groups)
     for digit_group in digit_groups:
-        plt.hist(df[df[last_digit_colname_prefix +
-                       party].isin(digit_group)][party],
-                 alpha=alpha, bins=bins)
+        if digit_group != _USE_MEAN_DIGIT_GROUP:
+            plt.hist(df[df[last_digit_colname_prefix +
+                           party].isin(digit_group)][party],
+                     alpha=alpha, bins=bins)
+        else:
+            plt.hist(df[party],
+                     weights=[0.1] * len(df), alpha=alpha, bins=bins)
     if title is None:
         title = party
     plt.title(title)
+
+
+def _plot_party_vote_by_digit_relationships_with_average(
+        df, party, max_votes, last_digit_colname_prefix, n_bins
+):
+    f = plt.figure()
+    f.suptitle("Distribution of %s vote counts by last digit vs. their average "
+               "empiric distribution" % (party))
+    i = 1
+
+    def plot_to_next(l):
+        nonlocal i
+        f.add_subplot(4, 3, i)
+        i += 1
+        l()
+
+    for digit in range(10):
+        plot_to_next(
+            lambda: _plot_votes_of_digits_hist(
+                df, party, [[digit], 4.5], n_bins=n_bins,
+                title="%d" % digit,
+                max_votes=max_votes,
+                last_digit_colname_prefix=last_digit_colname_prefix
+            ),
+        )
+    plt.tight_layout()
+    plt.show()
 
 
 def plot_party_vote_by_digit_relationships(df, party="Fidesz", ref_digit=7,
@@ -35,7 +72,12 @@ def plot_party_vote_by_digit_relationships(df, party="Fidesz", ref_digit=7,
     :param max_votes:
     :return:
     """
-    assert ref_digit in range(0, 10)
+    assert ref_digit in list(range(0, 10)) + [_USE_MEAN_DIGIT_GROUP]
+    if ref_digit == _USE_MEAN_DIGIT_GROUP:
+        return _plot_party_vote_by_digit_relationships_with_average(
+            df, party, max_votes, last_digit_colname_prefix, n_bins
+        )
+
     f = plt.figure()
     f.suptitle("Distribution of %s vote counts ending in "
                "%ds vs. others, 2019 values" % (party, ref_digit))
@@ -64,9 +106,20 @@ def plot_party_vote_by_digit_relationships(df, party="Fidesz", ref_digit=7,
 if __name__ == "__main__":
     from preprocessing import get_preprocessed_data
     df = get_preprocessed_data()
-    plot_party_vote_by_digit_relationships(df, "Fidesz", max_votes=600)
-    plot_party_vote_by_digit_relationships(df, "DK", max_votes=600)
-    plot_party_vote_by_digit_relationships(df, "Momentum", max_votes=600)
+    # plot_party_vote_by_digit_relationships(df, "Fidesz", max_votes=600)
+    # plot_party_vote_by_digit_relationships(df, "DK", max_votes=600)
+    # plot_party_vote_by_digit_relationships(df, "Momentum", max_votes=600)
+
+    plot_party_vote_by_digit_relationships(df, "Fidesz", max_votes=600,
+                                           ref_digit=_USE_MEAN_DIGIT_GROUP,
+                                           n_bins=60)
+    plot_party_vote_by_digit_relationships(df, "DK", max_votes=600,
+                                           ref_digit=_USE_MEAN_DIGIT_GROUP,
+                                           n_bins=60)
+    plot_party_vote_by_digit_relationships(df, "Momentum", max_votes=600,
+                                           ref_digit=_USE_MEAN_DIGIT_GROUP,
+                                           n_bins=60)
+
     # plot_party_vote_by_digit_relationships(df, "Fidesz", max_votes=600, ref_digit=1)
     # plot_party_vote_by_digit_relationships(df, "DK", max_votes=600, ref_digit=1)
     # plot_party_vote_by_digit_relationships(df, "Fidesz", max_votes=600, ref_digit=0)
