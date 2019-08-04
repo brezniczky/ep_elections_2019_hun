65feefc4e3af202781d2142056fa1c194bfaabc4
Janos Brezniczky <brezniczky@gmail.com>
2019-08-04 23:32:12 +0100

    Digit equalities (+ earlier correlations) added

--- app14_digit_correlations_Hun.py
+++ app14_digit_correlations_Hun.py
@@ -1,12 +1,22 @@
 from preprocessing import get_preprocessed_data
 from digit_correlations import (
-    correlation_prob_coeff_df, equality_prob_coeff_df
+    correlation_prob_coeff_df, equality_prob_coeff_df,
+    get_matrix_lambda_num, get_col_lambda_num, get_col_mean_prob,
+    get_matrix_mean_prob
 )
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
 
 
 relevant_cols = ["Nevjegyzekben", "Ervenyes", "Fidesz", "Jobbik",
                  "LMP", "MSZP", "DK", "Momentum"]
+
+ACT_FILENAME = "app14_valid_votes_coincidences.csv"
+BASELINE_FILENAME_FORMAT = ("app14_simulated_baseline/" +
+                            "app14_valid_votes_coincidences_baseline_%d.csv")
+N_BASELINE_REPEATS = 200
 
 
 def print_probs(df):
@@ -103,6 +113,221 @@ def check_equalities():
     print_probs(equality_prob_coeff_df(df))
 
 
+def test_cross_equalities(df):
+    df = (
+        equality_prob_coeff_df(
+            )
+    )
+    df.rename(columns={
+        "ld_Nevjegyzekben": "ld_Nevj", "ld_Ervenyes": "ld_Erv",
+        "ld_Momentum": "ld_Mom", "ld_Mi Hazank": "ld_Mi Haz"},
+        inplace = True
+    )
+    return df
+
+
+def test_area(df, area, starts_with_only=False):
+    df_act = df[
+        df.Telepules.str.startswith(area)
+        if starts_with_only else
+        df[df.Telepules == area]
+    ]
+    df = (
+        equality_prob_coeff_df(
+        df_act[["ld_Fidesz", "ld_Jobbik", "ld_Mi Hazank", "ld_LMP", "ld_DK",
+                "ld_Ervenyes", "ld_Nevjegyzekben", "ld_MSZP", "ld_Momentum"]])
+    )
+    df.rename(columns={
+        "ld_Nevjegyzekben": "ld_Nevj", "ld_Ervenyes": "ld_Erv",
+        "ld_Momentum": "ld_Mom", "ld_Mi Hazank": "ld_Mi Haz"},
+        inplace = True
+    )
+    return df
+
+
+def check_equalities_2(settlement_filter = None, df = None):
+    if df is None:
+        df = get_preprocessed_data()
+    if settlement_filter is not None:
+        df = df[df.Telepules.isin(settlement_filter)]
+    np.random.seed(1235)
+    noise = np.random.choice(range(10), len(df)) - 5
+    df = df[df.Ervenyes + noise >= 100]
+    ans_rows = []
+    for settlement in set(df.Telepules):
+        df_act = df[df.Telepules == settlement]
+        act_min_Ervenyes = min(df_act.Ervenyes)
+
+        if len(df_act) < 8:
+            continue
+        if act_min_Ervenyes < 100:
+            continue
+
+        df_act = df_act[
+            ["ld_Nevjegyzekben", "ld_Ervenyes",
+             "ld_Fidesz", "ld_DK", "ld_Momentum", "ld_LMP", "ld_Jobbik",
+             "ld_MSZP"]
+        ]
+        test_res = equality_prob_coeff_df(df_act)
+        lam = get_matrix_lambda_num(test_res)
+        erv_lam = get_col_lambda_num(test_res, "ld_Ervenyes")
+        erv_mean_prob = get_col_mean_prob(test_res, "ld_Ervenyes")
+        fidesz_mean_prob = get_col_mean_prob(test_res, "ld_Fidesz")
+        mean_prob = get_matrix_mean_prob(test_res)
+        row = test_res["ld_Ervenyes"]
+        row.index = ["ld_Erv_" + str(k) for k in row.index]
+        row2 = test_res["ld_Fidesz"]
+        row2.index = ["ld_Fid_" + str(k) for k in row2.index]
+        row = row.to_dict()
+        row.update(row2.to_dict())
+        row["lambda"] = lam
+        row["Erv_lambda"] = erv_lam
+        row["Erv_mean_prob"] = erv_mean_prob
+        row["Fidesz_mean_prob"] = fidesz_mean_prob
+        row["mean_prob"] = mean_prob
+        row["Telepules"] = settlement
+        row["n_wards"] = len(df_act)
+        row["min_ervenyes"] = act_min_Ervenyes
+        ans_rows.append(row)
+        print(row)
+
+    return pd.DataFrame(ans_rows)
+
+
+def get_baseline_filename(i):
+    return BASELINE_FILENAME_FORMAT % i
+
+
+def get_act_data():
+    if not os.path.exists(ACT_FILENAME):
+        df_act = check_equalities_2()
+        df_act.to_csv(ACT_FILENAME, index=False)
+    else:
+        df_act = pd.read_csv(ACT_FILENAME)
+    return df_act
+
+
+def require_simulations():
+    for i in range(N_BASELINE_REPEATS):
+        filename = get_baseline_filename(i)
+        if os.path.exists(filename):
+            continue
+
+        df = get_preprocessed_data()
+
+        def random_digits_df():
+            nonlocal df
+            return np.random.choice(range(10), len(df))
+
+        np.random.seed(1234 + i)
+        df.ld_Momentum = random_digits_df()
+        df.ld_DK = random_digits_df()
+        df.ld_LMP = random_digits_df()
+        df.ld_Jobbik = random_digits_df()
+        df.ld_Momentum = random_digits_df()
+        df.ld_MSZP = random_digits_df()
+        df.ld_Ervenyes = random_digits_df()
+
+        df = check_equalities_2(df=df)
+        df.to_csv(filename,
+                  index=False)
+
+
+def plot_equality_tests():
+
+    def get_mean_mean_prob(test_results):
+        p1 = test_results.Erv_mean_prob
+        p2 = test_results.Fidesz_mean_prob
+        # probably this should be better as a geom. mean too but ... approx.
+        # equal I guess due to the value range (from experience) anyway and I'm
+        # lazy
+        return np.mean(p1 * p2 ** 0.5)
+
+    mean_mean_probs = []
+    require_simulations()
+    for i in range(N_BASELINE_REPEATS):
+        df = pd.read_csv(get_baseline_filename(i))
+        mean_mean_prob = get_mean_mean_prob(df)
+        mean_mean_probs.append(mean_mean_prob)
+
+    df_act = get_act_data()
+    mean_mean_prob = get_mean_mean_prob(df_act)
+
+    hh = plt.hist(mean_mean_probs, bins=30)
+    plt.title("Mean probability distribution: simulated vs. actual")
+    plt.axvline(x=mean_mean_prob, color="red")
+    plt.text(mean_mean_prob + 0.0005, max(hh[0]) * 0.95,
+             "P=%.2f %%" %
+             (100 * sum(mean_mean_probs <= mean_mean_prob) /
+              len(mean_mean_probs)))
+    plt.show()
+
+
+def get_baseline_probs():
+
+    for i in range(N_BASELINE_REPEATS):
+        df = pd.read_csv(get_baseline_filename(i))
+        # df.set_index(["Telepules"])
+        if i == 0:
+            if sum(df["Fidesz_mean_prob"].values == 0) > 0:
+                import ipdb; ipdb.set_trace()
+            if sum(df["Fidesz_mean_prob"].values == 0) > 0:
+                import ipdb;
+            Fid_probs = np.log(df["Fidesz_mean_prob"].values)
+            Erv_probs = np.log(df["Erv_mean_prob"].values)
+        else:
+            if sum(df["Fidesz_mean_prob"].values == 0) > 0:
+                import ipdb; ipdb.set_trace()
+            if sum(df["Fidesz_mean_prob"].values == 0) > 0:
+                import ipdb;
+            Fid_probs += np.log(df["Fidesz_mean_prob"].values)
+            Erv_probs += np.log(df["Erv_mean_prob"].values)
+
+    return np.exp((Fid_probs + Erv_probs) / N_BASELINE_REPEATS / 2)
+
+
+def get_top_list(n_top=None):
+    df_act = get_act_data()
+    baseline_probs = get_baseline_probs()
+
+    top_list = df_act[["Telepules", "mean_prob"]].copy()
+
+    top_list["rel_mean_prob"] = top_list["mean_prob"] / baseline_probs
+
+    top_list = top_list.sort_values(["rel_mean_prob"])
+    if n_top is not None:
+        top_list = top_list.head(n_top)
+    top_list.reset_index()
+    top_list.index = range(1, len(top_list) + 1)
+    return df_act, top_list
+
+
+def get_digit_freqs():
+    df = get_preprocessed_data()
+    np.random.seed(4321)
+    df = df[df.Fidesz >= 100 + np.random.choice(range(10), len(df)) - 5]
+
+    suited = df[["Telepules", "Fidesz"]].groupby(["Telepules"]).aggregate(
+        len).reset_index()
+    suited = suited[suited.Fidesz >= 8].Telepules
+
+    df = df[df.Telepules.isin(suited.values)]
+
+    ld_Fidesz_freqs = np.unique(df.ld_Fidesz, return_counts=True)[1]
+    ld_Fidesz_freqs = ld_Fidesz_freqs / sum(ld_Fidesz_freqs)
+    ld_Erv_freqs = np.unique(df.ld_Ervenyes, return_counts=True)[1]
+    ld_Erv_freqs = ld_Erv_freqs / sum(ld_Erv_freqs)
+
+    return ld_Fidesz_freqs, ld_Erv_freqs
+
+
 if __name__ == "__main__":
     # check_correlations()
-    check_equalities()
+    # check_equalities()
+
+    # df = check_equalities_2(
+    #     settlement_filter=["Eger", "Tata", "Sopron", "Gyöngyös", "Keszthely"]
+    # )
+    # plot_equality_tests()
+    # df, df_top_list = get_top_list()
+    print(get_top_list())
--- digit_correlations.py
+++ digit_correlations.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import numpy.random as rnd
+from scipy import stats
 from functools import lru_cache
 
 
@@ -30,7 +31,7 @@ def equality_prob(a1: np.array, a2: np.array):
 
 
 @lru_cache(100)
-def digit_equality_prob_cdf(n_digits, seed=1234, n_iterations=50000):
+def digit_equality_prob_mc_cdf(n_digits, seed=1234, n_iterations=50000):
     corrs = []
     rnd.seed(seed)
     for i in range(n_iterations):
@@ -48,6 +49,21 @@ def digit_equality_prob_cdf(n_digits, seed=1234, n_iterations=50000):
     cdf.max_x = corrs[-1]
     cdf.min_x = corrs[0]
     return cdf
+
+
+def digit_equality_prob_analytical_cdf(n):
+    inner_cdf = stats.binom(n, 0.1).cdf
+
+    def cdf(rel_freq):
+        return inner_cdf(rel_freq * n)
+
+    return cdf
+
+
+"""
+faster, more accurate, no idea how to generalize for heterogeneous probabilities
+"""
+digit_equality_prob_cdf = digit_equality_prob_analytical_cdf
 
 
 def correlation_prob_coeff_df(df: pd.DataFrame):
@@ -86,8 +102,44 @@ def equality_prob_coeff_df(df: pd.DataFrame):
     return ans_df
 
 
+def get_matrix_lambda_num(df: pd.DataFrame) -> float:
+    """ Given a df with probabilities (outside its diagonals), calculate a
+        "wavelength" ("lambda") value. It's like a 'period of recurrence' - for
+        1:10 events, it would  be 10. Just adding the reciprocal of the
+        probabilities in the matrix that df is, outside the diagonal. """
+    return sum([1 / df[r][c] for r in df.index for c in df.columns if r != c])
+
+
+def get_col_lambda_num(df, col_name) -> float:
+    """ Similar to get_matrix_lambda_num, just a specified column.
+        Division by zero problems are intentionally left unhandled.
+    """
+    s = df[df.index != col_name][col_name]
+    return sum(1 / s)
+
+
+def get_col_mean_prob(df, col_name) -> float:
+    """ Return an average probability (geometric mean probability) generated
+        from the contents of the column, except for the "diagonal" value. """
+    s = np.exp(np.mean(np.log(
+        df[df.index != col_name][col_name].values.astype(float)
+    )))
+    return s
+
+
+def get_matrix_mean_prob(df: pd.DataFrame) -> float:
+    ans = np.exp(np.mean([np.log(df[r][c])
+                           for r in df.index
+                           for c in df.columns if r != c]))
+    return ans
+
+
 if __name__ == "__main__":
     cdf = digit_correlation_cdf(8531)
     print("probability correlations higher than 0.01985", 1 - cdf(0.01985))
-    cdf = digit_equality_prob_cdf(8531)
-    print("probability equalities higher than 0.1", 1 - cdf(0.1))
+    cdf = digit_equality_prob_mc_cdf(8531)
+    print("nonparam. probability equalities higher than 0.11", 1 - cdf(0.11))
+    cdf2 = digit_equality_prob_analytical_cdf(8531)
+    for i in range(100):
+        x = cdf2(0.11)
+    print("param. probability equalities higher than 0.11", 1 - cdf2(0.11))
