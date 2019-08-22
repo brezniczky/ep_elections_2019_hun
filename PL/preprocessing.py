de30a34144ecac135c2db1344384b1a3e6a56da7
Janos Brezniczky <brezniczky@gmail.com>
2019-08-22 01:40:30 +0100

    Polish data: improved preprocessing and uploaded to Kaggle

--- PL/preprocessing.py
+++ PL/preprocessing.py
@@ -1,34 +1,14 @@
-from typing import List
+from typing import List, Tuple
 import pandas as pd
 from functools import lru_cache
 import os.path
 from collections import OrderedDict
 
 
+_XLSX_FILENAME = "PL/wyniki_gl_na_kand_po_obwodach.xlsx"
 _AREA_CODE_COLNAME = "Kod terytorialny gminy"
 _VALID_VOTES_COLNAME = "Liczba kart ważnych"
 _VOTERS_ELIGIBLE_TO_VOTE_COLNAME = "Liczba wyborców uprawnionych do głosowania"
-
-
-def get_cleaned_data():
-    csv_filename = "PL/wyniki_gl_na_kand_po_obwodach_sheet_1.csv"
-    if os.path.exists(csv_filename):
-        try:
-            df = pd.read_csv(csv_filename)
-            return df
-        except Exception as e:
-            print("Error reading saved cleaned data, attempting to recreate ...")
-            print("  details:", str(e))
-    else:
-        print("Cleaning data...")
-
-    xlsx_filename = "PL/wyniki_gl_na_kand_po_obwodach.xlsx"
-    xls = pd.ExcelFile(xlsx_filename)
-    sheet_name = xls.sheet_names[0]
-    print("Reading sheet", sheet_name)
-    df = pd.read_excel(xlsx_filename, sheet_name=sheet_name)
-    df.to_csv(csv_filename, index=False)   # , encoding="utf-8")
-    return df
 
 
 @lru_cache(maxsize=1)
@@ -36,7 +16,15 @@ def get_xlsx(filename):
     return pd.ExcelFile(filename)
 
 
-def get_big_cleaned_data():
+def get_data_sheets() -> List[pd.DataFrame]:
+    """
+    Obtains the data on the constituent sheets that the Excel workbook. Sheet
+    data are slow to extract (in this way at least), so these are saved into a
+    multitude of csv files first. When already available, these are simply
+    loaded.
+
+    :return: List of data frames matching the sheets of the Excel file.
+    """
     sheet_list_csv_filename = "PL/wyniki_gl_na_kand_po_obwodach_sheet_list.csv"
     sheet_csv_filename = "PL/wyniki_gl_na_kand_po_obwodach_sheet_%s.csv"
 
@@ -48,7 +36,8 @@ def get_big_cleaned_data():
                 dfs.append(pd.read_csv(sheet_csv_filename % sheet_id))
             return dfs
         except Exception as e:
-            print("Error reading saved cleaned data, attempting to recreate ...")
+            print("Error reading saved cleaned data, attempting to "
+                  "recreate ...")
             print("  details:", str(e))
 
     print("Cleaning data...")
@@ -77,8 +66,8 @@ def get_big_cleaned_data():
     return dfs
 
 
-def get_lista_col_idx(col):
-    return int(col.split(" ")[2])
+def get_lista_col_idx(col_name):
+    return int(col_name.split(" ")[2])
 
 
 class MergedDataInfo():
@@ -109,6 +98,7 @@ class MergedDataInfo():
     nr_of_registered_voters_column = \
         property(lambda self: self._nr_of_registered_voters_column)  # type: str
 
+    """ Totals per lista. """
     lista_columns = \
         property(lambda self: self._lista_columns)  # type: List[str]
 
@@ -124,11 +114,12 @@ class MergedDataInfo():
 TODO: add another, per csv auto-selected column: that of the most popular candidate per lista
 """
 def merge_lista_results(dfs,
-                        # don't ask :) 7, 8, 9 looked too sparse I think
+                        # don't ask :) 7, 8, 9 looked too sparse I think anyway
                         # TODO: but #8 just fails with some error ;)
                         lista_idxs_to_exclude=[8],
                         return_overview_cols=False):
     """
+    Merges lista results splattered over the sheets into a unified data frame.
 
     :param dfs: The data frames to merge, consisting of standard and "lista"
         (candidate preference) columns.
@@ -137,11 +128,12 @@ def merge_lista_results(dfs,
         additionally. These will be described in the second member of the return
         tuple.
 
-    :return: A single data frame if return_overview_cols is False. Otherwise
-        a tuple of a data frame and a MergedListaInfo to help with navigating
-        around.
+    :return: A single data frame if return_overview_cols is False, with the
+        basic contents: area code, then lista total columns. Otherwise a tuple
+        of a more verbose data frame and a MergedListaInfo to help with
+        navigating around among its columns.
     """
-    print("merging \"lista\" columns...")
+    print("Merging \"lista\" columns...")
     lista_col_names = set()
 
     for df in dfs:
@@ -173,6 +165,7 @@ def merge_lista_results(dfs,
     merged = pd.concat(dfs_to_merge)
 
     for col in merged.columns:
+        # "last digit" columns
         if col.startswith("Lista"):
             merged["ld_" + col] = merged[col] % 10
 
@@ -187,3 +180,23 @@ def merge_lista_results(dfs,
             lista_columns=lista_col_names,
         )
         return merged, info
+
+
+def get_preprocessed_data() -> Tuple[pd.DataFrame, MergedDataInfo]:
+    """ Returns a data frame with the 2019 Polish EP election data, and some
+        metadata about it as the columns are left at their original names.
+
+        Remark: Lista 8 is currently excluded due to a likely pre-processing bug
+        causing an exception.
+
+        Expect the following columns:
+        - area code
+        - total votes per lista (except for Lista 8)
+        - total valid votes
+        - voters eligible to vote (registered)
+        - ld_...: one "last digit" column per each lista column for convenience
+
+        Currently likely the most convenient point to start analyzing the data.
+    """
+    dfs = get_data_sheets()
+    return merge_lista_results(dfs, return_overview_cols=True)
